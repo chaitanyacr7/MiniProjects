c127d7a511af30a79f2abedaf48842f7337ac203
Chaitanya Sankhe <csankhecr7@gmail.com>
2020-07-09 20:25:23 +0530

    Updated

---
+++ Counter/js/script.js
@@ -0,0 +1,68 @@
+// using objects
+var allfunctions = {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (function() {
+//     var navbar = document.getElementById("counterAllValues").innerText;
+//     var counter1 = document.getElementById("counterValue1").innerHTML;
+//     document.getElementById("addButton1").addEventListener("click", incrementCounter);
+//     document.getElementById("subtractButton1").addEventListener("click", decrementCounter);
+//     document.getElementById("resetButton").addEventListener("click", reset);
+//     document.getElementById("deleteButton1").addEventListener("click", deleteitem);
+//     var removeitem = document.getElementById("item1");
+
+//     function incrementCounter() {
+//         counter1++;
+//         document.getElementById("counterValue1").innerHTML = counter1;
+//     }
+
+//     function decrementCounter() {
+//         if (counter1 > 0) {
+//             counter1--;
+//             document.getElementById("counterValue1").innerHTML = counter1;
+//         }
+//     }
+
+//     function reset() {
+//         counter1 = 0;
+//         document.getElementById("counterValue1").innerHTML = counter1;
+//     }
+
+//     function deleteitem() {
+//         removeitem.remove()
+//     };
+
+// })();
---
+++ Validation Form/js/script2.js
@@ -0,0 +1,6 @@
+class people {
+    constructor() {
+
+    }
+}
+people.ca
